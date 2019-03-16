69319ff624c23703e85dff3e97d9395df7912084
真田雄太 <sndyut0089@gmail.com>
2019-03-17 01:56:27 +0900

    create GlobalSatte

--- Redux/State/GlobalState.swift
+++ Redux/State/GlobalState.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import ReSwift
+import API
+
+public struct GlobalState: ReSwift.StateType {
+    public typealias ThisState = GlobalState
+    public enum AppTab {
+        case main
+        case favorite
+        case setting
+        
+        public func isSelected(with appTab: AppTab) -> Bool {
+            return self == appTab
+        }
+    }
+    public enum ApplicationState {
+        case willResignActive
+        case didEnterBackground
+        case willEnterForeground
+        case didBecomeActive
+        case willTerminate
+    }
+    private var appState: ApplicationState = .willTerminate
+    private var didBecomeActiveTrigger: ShouldTrigger?
+    public private(set) var selectedAddTab: AppTab = .main
+}
+
+extension GlobalState {
+    public enum Action: ReSwift.Action {
+        case changeAppTab(appTab: AppTab)
+        case changeApplicationState(appState: ApplicationState)
+    }
+}
+
+extension GlobalState {
+    public static func reducer(action: ReSwift.Action, state: ThisState) -> ThisState {
+        guard let action = action as? ThisState.Action else { return state }
+        var state = state
+        switch action {
+        case .changeAppTab(let appTab):
+            state.selectedAddTab = appTab
+        case .changeApplicationState(let appState):
+            state.appState = appState
+            switch appState {
+            case .didBecomeActive:
+                state.didBecomeActiveTrigger = ShouldTrigger()
+            default: break
+            }
+        }
+        return state
+    }
+}
